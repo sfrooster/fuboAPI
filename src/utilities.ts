d6fea33300936aa8a5bde7dbe94e98f39a2d23a0
Michael Haan <haanmd@gmail.com>
2021-08-04 02:00:23 -0700

    reorder for sort

--- src/utilities.ts
+++ src/utilities.ts
@@ -13,14 +13,14 @@ export const isError = util.types.isNativeError;
 export const sleep = (millisToSleep: number) => new Promise(resolve => setTimeout(resolve, millisToSleep));
 
 export class Settings {
-    private static readonly _projectRoot = __dirname;
-    public static get projectRoot() {
-        return Settings._projectRoot;
-    }
-
     private static readonly _debug = ['1', 'true'].includes((process.env.FUBO_DEBUG ?? 'false').toLocaleLowerCase());
     public static get debug() {
         return Settings._debug;
+    }
+    
+    private static readonly _projectRoot = __dirname;
+    public static get projectRoot() {
+        return Settings._projectRoot;
     }
 
     private constructor() {}
