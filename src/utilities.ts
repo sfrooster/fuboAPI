b1ca753fafb14ae4ed75561f755a0105abd9b893
Michael Haan <haanmd@gmail.com>
2021-08-04 01:47:17 -0700

    additional Settings, utilities to support staggered data store starts and a sleep capability, swwitch from a customer isError to the Node native version

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,11 +1,26 @@
+import * as util from 'util';
 
-export const isError = (candidate: any): candidate is Error => candidate instanceof Error;
+
+
+export const getRandomIntInclusive = (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const isError = util.types.isNativeError;
+
+export const sleep = (millisToSleep: number) => new Promise(resolve => setTimeout(resolve, millisToSleep));
 
 export class Settings {
     private static readonly _projectRoot = __dirname;
     public static get projectRoot() {
-        console.log(`returing ${Settings._projectRoot} for projectRoot`);
         return Settings._projectRoot;
+    }
+
+    private static readonly _debug = ['1', 'true'].includes((process.env.FUBO_DEBUG ?? 'false').toLocaleLowerCase());
+    public static get debug() {
+        return Settings._debug;
     }
 
     private constructor() {}
